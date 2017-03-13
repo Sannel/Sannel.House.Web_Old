56a7fc895644298de0ecd872f541d039520955af
Adam Holt <holtsoftware@outlook.com>
2017-03-12 18:59:27 -0600

    Configuration Scss

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,8 +3,18 @@ var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var ts = require("gulp-typescript");
 var sourcemaps = require("gulp-sourcemaps");
+var runSequence = require("run-sequence");
+var sass = require("gulp-sass");
+var debug = require("gulp-debug");
 
 gulp.task("default", function (callback) {
+	return runSequence(
+		"Compile-Typescript",
+		"Compile-SCSS",
+		callback);
+});
+
+gulp.task("Compile-Typescript", function (callback) {
 	var source = ["src/**/ts/**/*.ts","!**/*.d.ts", "!src/**/bin/**/*", "!src/**/obj/**/*"];
 	return gulp.src(source)
 		.pipe(plumber())
@@ -17,6 +27,13 @@ gulp.task("default", function (callback) {
 		.pipe(gulp.dest("src\\Sannel.House.Web\\wwwroot\\js"));
 });
 
-gulp.task("extra", function (callback) {
-
+gulp.task("Compile-SCSS", function (callback) {
+	var source = "./src/Sannel.House.Web/wwwroot/scss/Site.scss";
+	gulp.src(source)
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(sourcemaps.write())
+		.pipe(debug({ title: "Compiling "}))
+		.pipe(gulp.dest("src\\Sannel.House.Web\\wwwroot\\css"));
 });
