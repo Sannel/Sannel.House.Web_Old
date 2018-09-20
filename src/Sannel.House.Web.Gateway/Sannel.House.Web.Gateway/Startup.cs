f83649a46420e5c155ac55861f21bb4962753fed
Adam Holt <holtsoftware@outlook.com>
2018-09-19 22:48:13 -0600

    Added Sensors project. Verified i got authentication working correctly through gateway.

--- src/Sannel.House.Web.Gateway/Sannel.House.Web.Gateway/Startup.cs
+++ src/Sannel.House.Web.Gateway/Sannel.House.Web.Gateway/Startup.cs
@@ -10,21 +10,13 @@
    limitations under the License.*/
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using Sannel.House.Web.Users;
 using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
-using Microsoft.EntityFrameworkCore;
 using IdentityServer4.AccessTokenValidation;
 
 namespace Sannel.House.Web.Gateway
